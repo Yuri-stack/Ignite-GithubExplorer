98fc46e48c1d78ca9a29a9d544abcc0944704da8
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-22 17:02:48 -0300

    Config.: Configuring Scripts for Dev and Production Environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path')                            // forma de configurar caminhos independente do SO
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
     entry: path.resolve(__dirname, 'src', 'index.jsx'), // informa qual o arquivo principal
     output: {                                           // informa a saida do arquivo que será gerado
         path: path.resolve(__dirname, 'dist'),          // informa a pasta
