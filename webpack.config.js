99d616805633c773ef500d61b5d14c8b213848df
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-23 22:03:26 -0300

    Feat.: Add Fast Refresh in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')                            // forma de configurar caminhos independente do SO
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -18,18 +19,27 @@ module.exports = {
         static: {
             directory: path.join(__dirname, 'public')
         },
+        hot: true,
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HTMLWebpackPlugin({
             template: path.resolve(__dirname, 'public', 'index.html')
         })
-    ],
-    module: {                                           // como o app vai lidar quando importarmos cada tipo de arquivo
+    ].filter(Boolean),
+    module: {                       // como o app vai lidar quando importarmos cada tipo de arquivo
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: 'babel-loader'
+                use:{
+                    loader: 'babel-loader',
+                    options:{
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean)
+                    }
+                }
             },
             {
                 test: /\.scss$/,
