7b76eb18ffc18e8279f22d6fcd46470eaeb7fb9e
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-23 21:16:30 -0300

    Feat.: First React Component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,31 @@
+export function RepositoryList(){
+    return (
+        <section className="repositoy-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Descrição</p>
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Descrição</p>
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>Nome</strong>
+                    <p>Descrição</p>
+                    <a href="">
+                        Acessar Repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
