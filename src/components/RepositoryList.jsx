65475de65af0ff9ebc7fb203301ca2b2c9da25bd
Yuri Oliveira <yurioliveirasilva1999@gmail.com>
2021-08-27 23:20:44 -0300

    Feat.: Using useEffect to fetch repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
+
+//https://api.github.com/users/yuri-stack/repos
 
 const repository = {
     name: 'unform',
@@ -9,6 +12,15 @@ const repository = {
 }
 
 export function RepositoryList(){
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        // Fetch: verificar documentação
+        fetch('https://api.github.com/users/yuri-stack/repos')
+            .then(resp => resp.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repositoy-list">
             <h1>Lista de Repositórios</h1>
